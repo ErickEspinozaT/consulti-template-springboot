ed8a1bee4c5df37052f40ba8344ec91b1ce2b0f8
unknown <michaeljr_15@hotmail.com>
2021-04-23 23:40:16 -0500

    cuarto-commit

--- proyectotarea/src/main/java/com/example/proyectotarea/ProyectotareaApplication.java
+++ proyectotarea/src/main/java/com/example/proyectotarea/ProyectotareaApplication.java
@@ -2,8 +2,11 @@ package com.example.proyectotarea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan({"com.example.proyectotarea.controller.InicioController.java"})
 public class ProyectotareaApplication {
 
 	public static void main(String[] args) {
--- proyectotarea/src/main/java/com/example/proyectotarea/controller/InicioController.java
+++ proyectotarea/src/main/java/com/example/proyectotarea/controller/InicioController.java
@@ -1,14 +1,15 @@
 package com.example.proyectotarea.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class InicioController {
     
     @GetMapping("/")
     public String inicio(){
-        return "index";
+        return "inicio";
     }
     
 }
